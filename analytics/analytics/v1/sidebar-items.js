74676e9ca65ecd596013a36702acdd4a4ba0d100
Geordon Worley <vadixidav@gmail.com>
2017-04-29 04:27:28 -0400

    remove unecessary docs

--- analytics/analytics/v1/sidebar-items.js
+++ analytics/analytics/v1/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["get_events","`GET /`"],["record_event","`PUT /`"],["routes",""]],"static":[["static_rocket_route_info_for_get_events",""],["static_rocket_route_info_for_record_event",""]],"struct":[["Analytics",""]],"type":[["Data",""]]});
+initSidebarItems({"fn":[["get_events","`GET /`"],["record_event","`PUT /`"]],"static":[["static_rocket_route_info_for_get_events",""],["static_rocket_route_info_for_record_event",""]]});
